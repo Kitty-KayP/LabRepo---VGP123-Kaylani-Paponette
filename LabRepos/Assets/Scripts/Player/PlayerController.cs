c90feef6c8d85aeac02d8fef22e588e503b9daf5
Kitty-KayP <di.paponette@gmail.com>
2024-07-23 14:35:31 -0300

    Lab 3 completed

--- LabRepos/Assets/Scripts/Player/PlayerController.cs
+++ LabRepos/Assets/Scripts/Player/PlayerController.cs
@@ -1,28 +1,110 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody2D))]
-public class NewBehaviourScript : MonoBehaviour
+[RequireComponent(typeof(Rigidbody2D), typeof(SpriteRenderer), typeof(Animator))]
+public class PlayerController : MonoBehaviour
+    
 {
-    public float speed;
+    //Movement Variables
+    [SerializeField, Range (1, 20)] private float speed = 5;
+    [SerializeField, Range(1, 20)] private float jumpForce = 10;
+    [SerializeField, Range(0.01f, 1)] private float groundCheckRadius = 0.02f;
+    [SerializeField] private LayerMask isGroundLayer;
+ 
+
+    //GroundCHeck Stuff 
+    private Transform groundCheck;
+    private bool isGrounded = false;
+
+    //Component References 
         
     Rigidbody2D rb;
+    SpriteRenderer sr;
+    Animator anim;
 
-    // Start is called before the first frame update
+        // Start is called before the first frame update
     void Start()
     {
+        //Component References Filled 
         rb = GetComponent<Rigidbody2D>();
-        Debug.Log(rb.name);
-        
+        sr = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
+
+        // Debug.Log(rb.name);
+
+        //Checking values to ensure non garbage data
+        if (speed <= 0)
+        {
+            speed = 5;
+            Debug.Log("Speed was set Incorrectly");
+        }
+
+        if (jumpForce <= 0)
+        {
+            jumpForce = 10;
+            Debug.Log("jumpForce was set Incorrectly");
+        }
+
+        //Creating groundcheck object
+        if (!groundCheck)
+        {
+            GameObject obj = new GameObject();
+            obj.transform.SetParent(transform);
+            obj.transform.localPosition = Vector3.zero;
+            obj.name = "GroundCheck";
+            groundCheck = obj.transform;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
+        //Create a small overlap collider to check if we are touching the ground
+        IsGrounded();
+
+        //grab horizontal axis - Check Project Settings > Input Manage4r to see the inputs defined 
         float hInput = Input.GetAxis("Horizontal");
 
+
         rb.velocity = new Vector2(hInput * speed, rb.velocity.y);
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+
+        if (Input.GetButtonDown("Fire1") && isGrounded)
+        {
+            anim.SetTrigger("isAttacking");
+        }
+
+        if (Input.GetButtonDown("Fire1") && !isGrounded)
+        {
+            anim.SetTrigger("isJumpAttacking");
+        }
+
+        
+
+        if (hInput != 0) sr.flipX = (hInput < 0);
+        //if (hInput > 0 && sr.flipX || hInput < 0 && !sr.flipX) sr.flipX = !sr.flipX; {
+
+        anim.SetFloat("hInput", Mathf.Abs(hInput));
+        anim.SetBool("isGrounded", isGrounded);
+        
+    }
+    void IsGrounded()
+    {
+        if (!isGrounded)
+        {
+            if (rb.velocity.y <= 0)
+            {
+                isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, isGroundLayer);
+            }
+        }
+        else
+            isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, isGroundLayer);
     }
 }
